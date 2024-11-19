daf8b0cb3cc94d931f100a4d25909ffe7faeab77
nicknt <nikolasderm@gmail.com>
2024-11-19 15:26:40 +0200

    change the first function

--- lab04.s
+++ lab04.s
@@ -18,9 +18,13 @@ arrayNotSorted: .word marianna, markos, maria
 
 .text
 
-            la   a0, arrayNotSorted
-            li   a1, 4
-            jal  recCheck
+            la   a0, maria
+            la   a1, markos
+            jal str_ge
+                        
+            #la   a0, arrayNotSorted
+            #li   a1, 4
+            #jal  recCheck
 
             li   a7, 10
             ecall
@@ -30,7 +34,25 @@ str_ge:
 # Write the subroutine code here
 #  You may move jr ra   if you wish.
 #---------
-            jr   ra
+    add s0, a0, zero
+    add s1, a1, zero
+loop:
+    lb t0, 0(s0)
+    lb t1, 0(s1)
+    beq t0, zero, exitLoop
+    beq t1, zero, exitLoop
+    blt t0, t1, greaterThan
+    addi s0, s0, 1
+    addi s1, s1, 1
+    j loop
+
+greaterThan:
+    addi a0, zero, 1
+    jr   ra
+
+exitLoop:
+    add a0, zero, zero 
+    jr   ra
  
 # ----------------------------------------------------------------------------
 # recCheck(array, size)
